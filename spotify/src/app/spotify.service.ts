836f6c3bcac73f0ccf68270bba9e57f36277a1b6
MaldifassiJacopo <maldifassi.jacopo@iisgalvanimi.edu.it>
2024-01-25 08:47:54 +0000

    Finito

--- spotify/src/app/spotify.service.ts
+++ spotify/src/app/spotify.service.ts
@@ -14,7 +14,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQBlBu5GfxSGN9W9QyUM_R5E72O-SJbZi9lWhKQ3yMH_xosHc9tiYHD_dwfjynZlafo0KV3yt0ebteNcNHqtrj4vJufsggohBpnJ5Qm3XhW8sI3nRxeV37rbwhLazJqfrC06aD75pZMp9i5i1qRnT6nedK2a-NAoIHRvwGzP747eLDpBApSclLt2h96fcoXbgtA'
+        'Bearer BQB4IJj56_w2TFQ9QrJ2av1T6Ip6sDHDZxwFrSG_FWsHVt5r9G349W0AOP_g1SKsqIwfAd37YoDgcyOS9meOOoKkuhxDJ2jjQgiHah93YFjVeiyew3tnroAAtX7aUzAksZGV7tqZFP2uc70IN--bHTc1fD0UzIb8SqyKvhSWNfSSUcDoBzI'
     });
 
     let obsTracks = this.http.get(url, { headers });
